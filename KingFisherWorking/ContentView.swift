d43357eb2cf0333369eab04de82857f60af7179c
Amish Tufail <amish.tufail2002@gmail.com>
2023-01-26 18:32:22 +0500

    - Different Methods of King Fisher

--- KingFisherWorking/ContentView.swift
+++ KingFisherWorking/ContentView.swift
@@ -6,14 +6,37 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10.0) {
+            // 1. KFImage
+            KFImage(URL(string: "https://picsum.photos/200"))
+            
+            // 2. KFImage.url
+            // 3. Image Processor
+            KFImage.url(URL(string: "https://picsum.photos/200"))
+                .setProcessor(ResizingImageProcessor(referenceSize: .init(width: 100, height: 100)))
+                .setProcessor(RoundCornerImageProcessor(cornerRadius: 12.0))
+            
+            // 4. On Progress > Tracks the current progess of the download
+            // 5. onSuccess and onFailure
+            KFImage.url(URL(string: "https://picsum.photos/200"))
+                .onProgress { recieved, total in
+                    print("\(recieved) - \(total)")
+                }
+                .onSuccess { data in
+                    print("Success")
+                }
+                .onFailure { data in
+                    print("Failure")
+                }
+            
+            // 6. Cache and Loading Option
+            KFImage.url(URL(string: "https://picsum.photos/200"))
+                .loadDiskFileSynchronously()
+//                . different cache methods
         }
         .padding()
     }
