71616e7784c5ed3005ac9137dc06d0b9d02d665c
Amish Tufail <amish.tufail2002@gmail.com>
2023-01-26 18:20:11 +0500

    Initial Commit

---
+++ KingFisherWorking/KingFisherWorkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  KingFisherWorkingApp.swift
+//  KingFisherWorking
+//
+//  Created by Amish Tufail on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct KingFisherWorkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
